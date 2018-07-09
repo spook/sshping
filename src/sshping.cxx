ee2fcf528dc424f4f63facee7a6c2e27464b2563
Thomas Gately <20bottless@gmail.com>
2018-07-09 11:46:28 +0100

    addding a Replacement for the strsep UNIX method

--- src/sshping.cxx
+++ src/sshping.cxx
@@ -178,6 +178,21 @@ DWORD getpid() {
 	return GetCurrentProcessId();
 }
 
+// Replacement for the strsep UNIX method
+char* strsep(char** stringp, const char* delim) {
+	char* start = *stringp;
+	char* p;
+	p = (start != NULL) ? strpbrk(start, delim) : NULL;
+	if (p == NULL) {
+		*stringp = NULL;
+	}
+	else {
+		*p = '\0';
+		*stringp = p + 1;
+	}
+	return start;
+}
+
 #endif
 
 // Format integers with delimiters
